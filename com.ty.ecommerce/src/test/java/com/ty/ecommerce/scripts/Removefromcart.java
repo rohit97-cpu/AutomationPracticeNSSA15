5f1960108dd7c00e95284598bfbc9ed7b7bbe045
Admin <Admin@DESKTOP-166QVCJ>
2021-05-31 16:15:29 +0530

    Changes made to Removefromcart

--- com.ty.ecommerce/src/test/java/com/ty/ecommerce/scripts/Removefromcart.java
+++ com.ty.ecommerce/src/test/java/com/ty/ecommerce/scripts/Removefromcart.java
@@ -1,9 +1,15 @@
 package com.ty.ecommerce.scripts;
 
+import org.testng.annotations.Test;
+
 import com.ty.ecommerce.genericlibs.Baseclass;
 
 public class Removefromcart extends Baseclass 
 {
-	
+	@Test
+	public void removefromcart()
+	{
+		
+	}
 
 }
